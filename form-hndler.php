a201ea616da404796367ad118423f18f4330b425
Shayan Azfar <111425128+SabaAzfar@users.noreply.github.com>
2024-04-26 07:18:45 +0500

    Update form-hndler.php

--- form-hndler.php
+++ form-hndler.php
@@ -5,7 +5,7 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 
-$email_from = 'info@yourwebsite.com'
+$email_from = 'info@info@sabaazfar.github.io/Hilt.pk'
 
 $email_subject = 'New Form Submission'
 
